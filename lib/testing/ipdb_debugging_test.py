50da4aa27310bddf5e3800c2ef6650584314f4a7
Habsa Abdirizack <habsa.abdirizack@student.moringaschool.com>
2024-11-28 18:09:35 +0300

    assignment

--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
-
-from ipdb_debugging import plus_two
+import pytest
+from ipdb_debugging import plus_two 
 
 class TestIpdbDebugging:
-    '''ipdb_debugging.py'''
-    
     def test_adds_two(self):
         '''adds_two() adds 2 to input arg and returns sum.'''
-        assert(plus_two(3) == 5)
+        assert(plus_two(3) == 5)  
